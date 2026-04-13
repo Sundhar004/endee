5e3e9ab509f7cdd16e4aa0bf5fdc085dc3a86ba2
Sundhar004 <sundhararameshwar.senthil@gmail.com>
2026-04-13 20:12:12 +0530

    Create chunking.py

---
+++ endee-rag-assistant/app/utils/chunking.py
@@ -0,0 +1,7 @@
+def chunk_text(text, chunk_size=500, overlap=50):
+    chunks, start = [], 0
+    while start < len(text):
+        end = min(start + chunk_size, len(text))
+        chunks.append(text[start:end].strip())
+        start = end - overlap
+    return chunks
